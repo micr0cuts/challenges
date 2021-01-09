4c9d72f106ba1efb324f3ef119c4c96204c4eefc
micr0cuts <svandieken@gmail.com>
2021-01-08 19:57:51 -0800

    removed so much redundancy

--- advent-of-code-2015/solution15.py
+++ advent-of-code-2015/solution15.py
@@ -1,5 +1,3 @@
-from itertools import permutations
-
 kitchen = {}
 
 with open('input15.txt', encoding='utf8') as f:
@@ -15,8 +13,9 @@ with open('input15.txt', encoding='utf8') as f:
 toy_kitchen = {"Butterscotch": {"capacity": -1, "durability": -2, "flavor": 6, "texture": 3, "calories": 8},
 "Cinnamon": {"capacity": 2, "durability": 3, "flavor": -2, "texture": -1, "calories": 3}}
 
-def calculate_score(ingredients, ns, kitchen):
-    props = [k for k in kitchen[list(kitchen.keys())[0]] if k != 'calories']
+def calculate_score(ns, kitchen):
+    ingredients = sorted(list(kitchen.keys()))
+    props = [k for k in kitchen[ingredients[0]] if k != 'calories']
     s = 1
     scores = dict().fromkeys(props)
 
@@ -41,36 +40,30 @@ def calculate_score(ingredients, ns, kitchen):
         s *= max(0, v)
     return s
 
-assert calculate_score(["Butterscotch", "Cinnamon"], [44, 56], toy_kitchen) == 62842880
+assert calculate_score([44, 56], toy_kitchen) == 62842880
 
-def calculate_calories(ingredients, ns, kitchen):
+def calculate_calories(ns, kitchen):
+    ingredients = sorted(list(kitchen.keys()))
     cals = 0
     for n, ing in zip(ns, ingredients):
         cals += n*kitchen[ing]['calories']
     return cals
 
-assert calculate_calories(["Butterscotch", "Cinnamon"], [40, 60], toy_kitchen) == 500
-
-#all possible permutations of 1 to 4 ingredients in all possible combinations
-perms = []
-for i in range(1,5):
-    perms.append(permutations(kitchen.keys(), i))
+assert calculate_calories([40, 60], toy_kitchen) == 500
 
 highest = 0
 cal_highest = 0
-for order in perms:
-    for group in order:
-        for i in range(0,101):
-            for j in range(0,101-i):
-                for k in range(0,101-i-j):
-                    l = 100-i-j-k
-                    score = calculate_score(group, [i, j, k, l], kitchen)
-                    # part 2
-                    if calculate_calories(group, [i, j, k, l], kitchen) == 500:
-                        if score > cal_highest:
-                            cal_highest = score
-                    if score > highest:
-                        highest = score
+for i in range(0,101):
+    for j in range(0,101-i):
+        for k in range(0,101-i-j):
+            l = 100-i-j-k
+            score = calculate_score([i, j, k, l], kitchen)
+            # part 2
+            if calculate_calories([i, j, k, l], kitchen) == 500:
+                if score > cal_highest:
+                    cal_highest = score
+            if score > highest:
+                highest = score
 
 print("The solution to part 1 is:", highest)
 print("The solution to part 2 is:", cal_highest)
