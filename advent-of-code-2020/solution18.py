4e88eda425efc465ad51d6305d92798806b6377a
micr0cuts <svandieken@gmail.com>
2021-01-12 21:10:04 -0800

    a go at part 2

--- advent-of-code-2020/solution18.py
+++ advent-of-code-2020/solution18.py
@@ -30,8 +30,7 @@ def find_brackets(equation):
     return bracket_pos
 
 def solve_one_by_one(equation):
-    print("in function", equation)
-    no_spaces = re.sub(' ', '', equation)
+    no_spaces = [i for i in equation if i != '']
     solution = no_spaces[0]
     for i in range(1, len(no_spaces), 2):
         # this doesn't work for integers over 9... because of len()
@@ -42,24 +41,87 @@ def solve_one_by_one(equation):
 def part1(inp):
     solved_equations = []
     for equation in inp:
-        equation = re.sub(' ', '', equation)
+        if type(equation) == str:
+            equation = list(re.sub(' ', '', equation))
 
         brackets = find_brackets(equation)
         new_equation = equation
         for bracket in brackets:
             sub_equation = new_equation[bracket[0]+1:bracket[1]]
-            print(new_equation)
             assert bracket_free(sub_equation), sub_equation
             partial_solution = solve_one_by_one(sub_equation)
             
-            new_equation = new_equation[:bracket[0]] + partial_solution + ' '*len(range(bracket[0], bracket[1]+1-len(partial_solution))) + new_equation[bracket[1]+1:]
+            new_equation = new_equation[:bracket[0]] + [partial_solution] + [' ' for i in range(bracket[0], bracket[1])] + new_equation[bracket[1]+1:]
             assert len(equation) == len(new_equation)
 
-        assert bracket_free(new_equation)
-        print("equation", equation)
-        print("new_equation", new_equation)
+        assert bracket_free(new_equation), new_equation
+
         solved_equations.append(int(solve_one_by_one(new_equation)))
     return sum(solved_equations)
 
 assert part1(toy_cases) == sum(toy_answers)
 print("The solution for part 1 is:", part1(inp))
+
+# part 2
+toy_answers2 =  [231, 51, 46, 1445, 669060, 23340]
+
+def add_brackets(equation):
+
+    plusses = [i for i, c in enumerate(equation) if c == '+']
+    for idx in plusses:
+        insert = []
+        opened = 0
+        # search left
+        for i, c in enumerate(equation[idx-1::-1]):
+            if c == ')':
+                opened -= 1
+            elif c == '(':
+                opened += 1
+            if opened == 0:
+                insert.append(idx-i+len(insert))
+                break
+        # search right
+        assert len(insert) % 2 == 1
+        assert opened == 0
+        for i, c in enumerate(equation[idx+1:]):
+            if c == '(':
+                opened += 1
+            elif c == ')':
+                opened -= 1
+            if opened == 0:
+                if equation[i+1] == '+':
+                    continue
+                insert.append(i+idx+1+len(insert))
+                break
+
+
+    # for i, c in enumerate(equation):
+    #     if c.isnumeric() and len(insert) %2 == 1:
+    #         insert.append(i+1+len(insert))
+    #     elif c == '+' and not opened:
+    #         if equation[i-1].isnumeric():
+    #             insert.append(i-1+len(insert))
+    #     elif c == '(' and insert:
+    #         opened += 1
+    #     elif c == ')' and insert:
+    #         opened -= 1
+    #         if opened == 0 and len(insert) % 2 == 1:
+    #             insert.append(i+1+len(insert))
+    opening = True
+    print(insert)
+    for i in insert:
+        if opening:
+            equation.insert(i, '(')
+        else:
+            equation.insert(i, ')')
+        opening = not opening
+    return equation
+
+def part2(inp):
+    new_inp = [add_brackets(list(re.sub(' ', '', equation))) for equation in inp]
+    print(new_inp)
+    return part1(new_inp)
+
+
+assert part2(toy_cases) == sum(toy_answers2)
+print("The solution for part 2 is:", part2(inp))
