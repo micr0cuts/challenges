db1dd95138705bb70e30f014fa9da449816589fd
micr0cuts <svandieken@gmail.com>
2021-02-18 14:23:03 -0800

    no idea what I' doing

--- advent-of-code-2016/day05.cc
+++ advent-of-code-2016/day05.cc
@@ -1,15 +1,19 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include "md5.h"
+#include <openssl/md5.h>
 
-std::string part1(std::string d) {
+std::string part1(const unsigned char d) {
     uint64_t n = 0;
     std::string password;
     while (true) {
-        std::string hashable = d + std::to_string(n);
-        std::string hash = MD5(hashable).hexdigest();
-        std::string substring = hash.substr(0,5);
+        const unsigned char* n_char;
+        n_char = (unsigned char*)n;
+        const unsigned char* hashable = d + n_char;
+        size_t m = sizeof(hashable);
+        unsigned char* md;
+        unsigned char* hash = MD5(hashable, m, md);
+        std::string substring = std::to_string(*hash).substr(0,5);
         if (substring.compare("00000") == 0) {
             password.push_back(hash[5]);
             if (password.size() == 8) {
@@ -21,7 +25,8 @@ std::string part1(std::string d) {
     
 }
 int main() {
-    std::string doorId = "ojvtpuvg";
-    std::string solution1 = part1(doorId);
+    const unsigned char* doorId;
+    doorId = (unsigned char*)"ojvtpuvg";
+    std::string solution1 = part1(*doorId);
     std::cout << solution1 << std::endl;
 }
