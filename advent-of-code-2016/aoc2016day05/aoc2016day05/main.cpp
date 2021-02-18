1f048541aa6f04fb46a41fd2a814f84ae64ccefb
micr0cuts <svandieken@gmail.com>
2021-02-17 23:05:37 -0800

    lots of compile errors, no idea

---
+++ advent-of-code-2016/aoc2016day05/aoc2016day05/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aoc2016day05
+//
+//  Created by Simon Vandieken on 2021-02-17.
+//  Copyright © 2021 Simon Vandieken. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ advent-of-code-2016/day05.cc
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include "md5.h"
+
+std::string part1(std::string d) {
+    uint64_t n = 0;
+    std::string password;
+    while (true) {
+        std::string hashable = d + std::to_string(n);
+        std::string hash = MD5(hashable).hexdigest();
+        std::string substring = hash.substr(0,5);
+        if (substring.compare("00000") == 0) {
+            password.push_back(hash[5]);
+            if (password.size() == 8) {
+                return password;
+            }
+        }
+        n++;
+    }
+    
+}
+int main() {
+    std::string doorId = "ojvtpuvg";
+    std::string solution1 = part1(doorId);
+    std::cout << solution1 << std::endl;
+}
