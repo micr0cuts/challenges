ae5625a9153230a6f2ee8aa306bfd3923f8cfd0a
micr0cuts <svandieken@gmail.com>
2021-02-18 22:02:50 -0800

    what is this magic

--- advent-of-code-2016/day07.py
+++ advent-of-code-2016/day07.py
@@ -1,5 +1,3 @@
-import re
-
 inp = [line.strip() for line in open('input07.txt', encoding='utf8')]
 
 TOY = ["abba[mnop]qrst",
@@ -16,21 +14,46 @@ def matches_abba(s):
                 return s[i] != s[i+1]
     return False
 
+def get_outer_inner(s):
+    inside = False
+    inner, outer = [], []
+    subs = ''
+    for n, char in enumerate(s):
+        if char == '[':
+            outer.append(subs)
+            subs = ''
+            inside = True
+        elif char == ']':
+            inner.append(subs)
+            subs = ''
+            inside = False
+        else:
+            subs = subs + char
+            if n == len(s) - 1:
+                outer.append(subs)
+    return outer, inner
+
 def part1(ips):
     tls_supported_ips = []
     for ip in ips:
         ans = False
-        in_brackets = re.findall('\\[.*?\\]', ip)
-        for i in in_brackets:
-            if matches_abba(i.strip('[').strip(']')):
-                break
-            outside_brackets = re.findall('(^[a-zA-Z]*)', ip)
-            outside_brackets += re.findall('(?<=\\])([a-zA-Z]*)', ip)
-            for o in outside_brackets:
-                if matches_abba(o):
-                    ans = True
-                    break
+        outer, inner = get_outer_inner(ip)
+        #### this magically works
+        if any(matches_abba(o) for o in outer):
+            if not any(matches_abba(i.strip('[').strip(']')) for i in inner):
+                ans = True
+        #### end
+        #### but this doesn't work??
+        # for i in inner:
+        #     if matches_abba(i.strip('[').strip(']')):
+        #         break
+        #     for o in outer:
+        #         if matches_abba(o):
+        #             ans = True
+        #             break
+        #### end
         tls_supported_ips.append(ans)
+
     return tls_supported_ips
 
 assert part1(TOY) == [True, False, False, True], part1(TOY)
