8758ac6978360888978608fc4875baf70d940ea7
micr0cuts <svandieken@gmail.com>
2021-12-01 21:55:21 -0800

    an idea from reddit

--- advent-of-code-2021/day02.py
+++ advent-of-code-2021/day02.py
@@ -8,27 +8,20 @@ tests: List = inputgetter_list('tests/02.txt')
 def solve(commands: List[str], part2: bool = False) -> int:
     aim: int = 0
     horizontal: int = 0
-    depth:int = 0
+    depth: int = 0
 
     for i in commands:
         cmd, val = i.split()
         val = int(val)
         if cmd == "forward":
             horizontal += val
-            if part2:
-                depth += aim*val
+            aim += depth*val
         elif cmd == "down":
-            if part2:
-                aim += val
-            else:
-                depth += val
+            depth += val
         elif cmd == "up":
-            if part2:
-                aim -= val
-            else:
-                depth -= val
+            depth -= val
 
-    return horizontal*depth
+    return horizontal*aim if part2 else horizontal*depth
 
 test_solution: int = solve(tests[:-1])
 print(test_solution, tests[-1])
