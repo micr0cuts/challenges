39554b22bf9f69cdf8f0459b1419078cb4f99090
micr0cuts <svandieken@gmail.com>
2021-12-01 21:26:05 -0800

    pathlib usage

--- advent-of-code-2021/lib.py
+++ advent-of-code-2021/lib.py
@@ -1,6 +1,10 @@
-def inputgetter_list(fname):
-    lines = []
-    with open(fname, encoding='utf8') as f:
+from typing import List
+
+from pathlib import Path
+
+def inputgetter_list(fname: str) -> List[str]:
+    lines: List = []
+    with Path(fname).open(encoding='utf8') as f:
         for line in f:
             lines.append(line.strip())
     return lines
