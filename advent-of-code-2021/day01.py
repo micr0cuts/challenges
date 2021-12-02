4628e449ac8e394b148a8931ce15a65e2702bc85
micr0cuts <svandieken@gmail.com>
2021-12-01 21:24:06 -0800

    some linting day01

--- advent-of-code-2021/day01.py
+++ advent-of-code-2021/day01.py
@@ -5,15 +5,16 @@ from lib import inputgetter_list
 inp: List = inputgetter_list('inputs/01.txt')
 tests: List = inputgetter_list('tests/01.txt')
 inp = [int(x) for x in inp]
+tests = [int(x) for x in tests]
 
-def solve(depth_measurements: List, part2: bool = False) -> int:
+def solve(depth_measurements: List[int], part2: bool = False) -> int:
     stride: int = 3 if part2 else 1
     prev_measurements: int = 0
     increased: int = 0
 
     for i in range(0, len(depth_measurements)):
         look_at: List = depth_measurements[i:i+stride]
-        the_sum: int = sum(x for x in look_at)
+        the_sum: int = sum(look_at)
 
         if i > 0:
             if the_sum > prev_measurements:
@@ -24,7 +25,7 @@ def solve(depth_measurements: List, part2: bool = False) -> int:
     return increased
 
 test_solution: int = solve(tests[:-1])
-assert  test_solution == int(tests[-1]), test_solution
+assert test_solution == tests[-1], test_solution
 
 solution1: int = solve(inp)
 print(f"The solution to part1 is: {solution1}")
