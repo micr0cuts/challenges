fba004ff1f5aa7f85242e36859f712c181daa509
micr0cuts <svandieken@gmail.com>
2021-12-11 13:30:30 -0800

    lint day 10

--- advent-of-code-2021/day10.py
+++ advent-of-code-2021/day10.py
@@ -1,13 +1,14 @@
+#pylint: disable=missing-module-docstring,missing-function-docstring,invalid-name
+from typing import List
+from typing import Set
+from typing import Tuple
 
 from lib import inputgetter_list
 
 sym2points = {')': 3, ']': 57, '}': 1197, '>': 25137}
-
-close2open = {')': '(', ']': '[', '}': '{', '>': '<'}
-
-open2close = {v: k for k, v in close2open.items()}
-
 sym2points2 = {'(': 1, '[': 2, '{': 3, '<': 4}
+close2open = {')': '(', ']': '[', '}': '{', '>': '<'}
+open2close = {v: k for k, v in close2open.items()}
 
 inp = inputgetter_list('inputs/10.txt')
 
@@ -16,41 +17,52 @@ tests_inp = inputgetter_list('tests/10.txt')
 opening = list(close2open.values())
 closing = list(close2open.keys())
 
-answers = 0
-corrupted = set()
-for i, line in enumerate(inp):
-    opened = []
-    for char in line.strip():
-        if char in opening:
-            opened.append(char)
-        if char in closing:
-            if char != open2close[opened[-1]]:
-                #print("Bad line", i, line, char)
-                answers += sym2points[char]
-                corrupted.add(i)
-                break
-            else:
-                opened.pop()
-print(answers)
+def solve(data: List[str]) -> Tuple[int, Set]:
+    score = 0
+    corrupted = set()
+    for i, line in enumerate(data):
+        opened = []
+        for char in line.strip():
+            if char in opening:
+                opened.append(char)
+            if char in closing:
+                # pylint: disable=no-else-break
+                if char != open2close[opened[-1]]:
+                    #print("Bad line", i, line, char)
+                    score += sym2points[char]
+                    corrupted.add(i)
+                    break
+                else:
+                    opened.pop()
+    return score, corrupted
 
-answers2 = []
-for i, line in enumerate(inp):
-    line_score = 0
-    if i in corrupted:
-        continue
-    opened = []
-    for char in line.strip():
-        if char in opening:
-            opened.append(char)
-        if char in closing:
-            if char == open2close[opened[-1]]:
-                opened.pop()
-    for char in opened[-1::-1]:
-        line_score *= 5
-        line_score += sym2points2[char]
-    answers2.append(line_score)
+tests_solution, test_corrupted = solve(tests_inp)
+assert tests_solution == 26397
+solution, corrupt = solve(inp)
+print(f"The solution to part 1 is: {solution}")
 
-answers2.sort()
-middle = (len(answers2)//2)
-print(answers2[middle])
+def solve2(data: List[str], filtered_inputs: Set[str]) -> int:
+    scores = []
+    for i, line in enumerate(data):
+        line_score = 0
+        if i in filtered_inputs:
+            continue
+        opened = []
+        for char in line.strip():
+            if char in opening:
+                opened.append(char)
+            if char in closing:
+                if char == open2close[opened[-1]]:
+                    opened.pop()
+        for char in opened[-1::-1]:
+            line_score *= 5
+            line_score += sym2points2[char]
+        scores.append(line_score)
 
+    scores.sort()
+    middle = (len(scores)//2)
+
+    return scores[middle]
+
+solution2 = solve2(inp, corrupt)
+print(f"The solution to part 1 is: {solution2}")
