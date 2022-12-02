b3d845fa0168f12de7838eac2383972a02089f29
micr0cuts <svandieken@gmail.com>
2022-12-01 21:49:44 -0800

    day 2, not satisfied

---
+++ advent-of-code-2022/day02.py
@@ -0,0 +1,71 @@
+from typing import List
+from lib import inputgetter_list
+
+inp: List = inputgetter_list('inputs/02.txt')
+tests: List = inputgetter_list('tests/02.txt')
+inp = [x.split() for x in inp]
+tests = [x.split() for x in tests]
+
+determine_score = {
+    "A": {"X": 3,
+          "Y": 6,
+          "Z": 0},
+    "B": {"X": 0,
+          "Y": 3,
+          "Z": 6},
+    "C": {"X": 6,
+          "Y": 0,
+          "Z": 3}
+}
+
+selection_score = {"X": 1, "Y": 2, "Z": 3}
+
+win_score = {"X": 0, "Y": 3, "Z": 6}
+
+def determine_win_part2(a, b):
+    if b == "X":
+        if a == "A":
+            return "Z"
+        if a == "B":
+            return "X"
+        if a == "C":
+            return "Y"
+    if b == "Y":
+        if a == "A":
+            return "X"
+        if a == "B":
+            return "Y"
+        if a == "C":
+            return "Z"
+    if b == "Z":
+        if a == "A":
+            return "Y"
+        if a == "B":
+            return "Z"
+        if a == "C":
+            return "X"
+
+def solve(inp):
+    my_score = 0
+    for opponent, my_choice in inp:
+        my_score += determine_score[opponent][my_choice]
+        my_score += selection_score[my_choice]
+    return my_score
+
+def solve2(inp):
+    my_score = 0
+    for opponent, end_result in inp:
+        my_choice = determine_win_part2(opponent, end_result)
+        my_score += win_score[end_result]
+        my_score += selection_score[my_choice]
+    return my_score
+
+test_solution = solve(tests)
+assert test_solution == 15
+solution1 = solve(inp)
+print(f"The solution to part 1 is: {solution1}")
+
+test_solution2 = solve2(tests)
+assert test_solution2 == 12
+solution2 = solve2(inp)
+print(f"The solution to part 2 is: {solution2}")
