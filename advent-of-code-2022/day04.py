523fd0762bdb3d2acff64bef8a0f1e24b51ac4af
micr0cuts <svandieken@gmail.com>
2022-12-04 13:02:40 -0800

    better solution using sets

--- advent-of-code-2022/day04.py
+++ advent-of-code-2022/day04.py
@@ -12,16 +12,20 @@ def solve(assignments: List[str], part2: bool = False) -> int:
         left, right = a.split(',')
         left_start, left_end = map(int, left.split('-'))
         right_start, right_end = map(int, right.split('-'))
+        left = list(range(left_start, left_end + 1))
+        right = list(range(right_start, right_end + 1))
 
         if part2:
-            left = list(range(left_start, left_end + 1))
-            right = list(range(right_start, right_end + 1))
-            overlaps += any(x for x in left if x in right)
+            # overlaps += any(x for x in left if x in right)
+            overlaps += len(set(left).intersection(right)) > 0
         else:
-            if left_start <= right_start and left_end >= right_end:
-                overlaps += 1
-            elif right_start <= left_start and right_end >= left_end:
-                overlaps += 1
+            # if left_start <= right_start and left_end >= right_end:
+            #     overlaps += 1
+            # elif right_start <= left_start and right_end >= left_end:
+            #     overlaps += 1
+            # second solution using sets
+            overlaps += set(left).issubset(right) or set(left).issuperset(right)
+
 
     return overlaps
 
